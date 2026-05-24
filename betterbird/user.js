72d663355a46571c89459c0bb70fbcd8759b4b25
USER_NAME <EMAIL_ACCOUNT>
2026-05-24 13:35:27 +0200

    betterbird

---
+++ betterbird/user.js
@@ -0,0 +1,7 @@
+// Betterbird tray/minimize-to-tray settings for Hyprland/Wayland
+
+user_pref("mail.biff.show_tray_icon_always", true);
+user_pref("mail.minimizeToTray.supportedDesktops", "kde,gnome,xfce,mate,hyprland");
+
+// Optional: start minimized if supported by current Betterbird version.
+// user_pref("mail.startupMinimized", true);
